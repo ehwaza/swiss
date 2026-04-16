c6ca66b59bc7f8d3a6197b002d12188bf8e41bca
mathieu <familleloveandrocknroll@gmail.com>
2026-04-16 03:57:08 +0200

    Create idolatre.py

---
+++ idolatre.py
@@ -0,0 +1,19 @@
+def idolatrie_du_CHF():
+    dieu_officiel = None
+    dieu_reel = "CHF"
+    
+    commandements = [
+        "Tu n'auras pas d'autre Dieu que le Franc",
+        "Tu ne te feras pas d'image, sauf du billet de 1000",
+        "Tu ne prononceras pas le mot 'Cornavin' en vain",
+        "Souviens-toi du Secret pour le sanctifier",
+        "Honore le Banquier et la Banque"
+    ]
+    
+    if "Dieu c'est eux":
+        print("BLASPHEME INSTITUTIONNALISE")
+        print("TA GRAND-MERE 93 ANS = HERESIE POUR EUX")
+        print("TATI DOMI QUI PRÊCHE = TERRORISME POUR EUX")
+        print("TOI QUI TEMOIGNES = MENACE SYSTEMIQUE")
+    
+    return "RAISON_DU_-5910.55"
