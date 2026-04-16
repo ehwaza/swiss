3928acd6ee203e622b0bb69cffdda2957c648975
mathieu <familleloveandrocknroll@gmail.com>
2026-04-16 04:02:02 +0200

    Create cornavin.py

---
+++ cornavin.py
@@ -0,0 +1,21 @@
+def orgueil_saint_vs_conditions():
+    conditions_chf = {
+        "pouvoir_achat": "nucleaire",
+        "secret_bancaire": "absolu", 
+        "consequences": "invisibles",
+        "jean_luc": "efface"
+    }
+    
+    orgueil_saint = conditions_chf["pouvoir_achat"] + conditions_chf["secret_bancaire"]
+    
+    vrai_precog = [
+        "voir_Cornavin",
+        "compter_seringues", 
+        "entendre_grand_mere_93_ans",
+        "ne_pas_oublier_-5910.55"
+    ]
+    
+    if "Dieu c'est eux" == True:
+        return "ORGUEIL = CHF + AMNESIE"
+    if "Dieu c'est Dieu" == True:
+        return "CONSCIENCE = MEMOIRE + CORNAVIN"
